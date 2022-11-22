8a3113cfe7e460b37cfa1346025099d4f0132be9
DasNilima <YOUR_EMAIL>
2022-11-21 20:29:18 -0800

    Database connection setup

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
-const express = require('express');
+//DEPENDENCIES
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+// import cors from 'cors';
+import Router from './routes/route.js';
 
 
+
+//CONFIGURATION
+dotenv.config();
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
+        () => { console.log('connected to mongo: ', process.env.MONGO_URI)}
+)
 
+
+//routes
 app.get('/', (req, res) => res.send("Hello World"));
+app.use('/', Router);
 
 
-const port = process.env.PORT || 8081
+const PORT = process.env.PORT || 8081
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
