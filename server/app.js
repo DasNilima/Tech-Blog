52b94763025d3c9012351ad794f7ef1bcb5c5a5d
DasNilima <YOUR_EMAIL>
2022-12-02 22:33:15 -0800

    update app.js

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(defineCurrentUser)
-
+app.use(express.static(path.resolve(__dirname, './client/build')))
 
 //routes
 app.get('/', (req, res) => res.send("Hello World"));
